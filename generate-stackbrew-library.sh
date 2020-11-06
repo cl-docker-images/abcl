949b25fb6a4e387d5dcc81dec683b38ba51c5aef
Eric Timmons <etimmons@mit.edu>
2020-11-06 18:26:13 -0500

    untabify scripts

--- generate-stackbrew-library.sh
+++ generate-stackbrew-library.sh
@@ -21,39 +21,39 @@ IFS=$'\n'; versions=( $(echo "${versions[*]}" | sort -rV) ); unset IFS
 
 # get the most recent commit which modified any of "$@"
 fileCommit() {
-	git log -1 --format='format:%H' HEAD -- "$@"
+    git log -1 --format='format:%H' HEAD -- "$@"
 }
 
 # get the most recent commit which modified "$1/Dockerfile" or any file COPY'd from "$1/Dockerfile"
 dirCommit() {
-	local dir="$1"; shift
-	(
-		cd "$dir"
-		fileCommit \
-			Dockerfile \
-			$(git show HEAD:./Dockerfile | awk '
-				toupper($1) == "COPY" {
-					for (i = 2; i < NF; i++) {
-						print $i
-					}
-				}
-			')
-	)
+    local dir="$1"; shift
+    (
+        cd "$dir"
+        fileCommit \
+            Dockerfile \
+            $(git show HEAD:./Dockerfile | awk '
+                toupper($1) == "COPY" {
+                    for (i = 2; i < NF; i++) {
+                        print $i
+                    }
+                }
+            ')
+    )
 }
 
 getArches() {
-	local repo="$1"; shift
-	local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
+    local repo="$1"; shift
+    local officialImagesUrl='https://github.com/docker-library/official-images/raw/master/library/'
 
-	eval "declare -g -A parentRepoToArches=( $(
-		find -name 'Dockerfile' -exec awk '
-				toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
-					print "'"$officialImagesUrl"'" $2
-				}
-			' '{}' + \
-			| sort -u \
-			| xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
-	) )"
+    eval "declare -g -A parentRepoToArches=( $(
+        find -name 'Dockerfile' -exec awk '
+                toupper($1) == "FROM" && $2 !~ /^('"$repo"'|scratch|.*\/.*)(:|$)/ {
+                    print "'"$officialImagesUrl"'" $2
+                }
+            ' '{}' + \
+            | sort -u \
+            | xargs bashbrew cat --format '[{{ .RepoName }}:{{ .TagName }}]="{{ join " " .TagEntry.Architectures }}"'
+    ) )"
 }
 getArches 'abcl'
 
@@ -65,14 +65,14 @@ EOH
 
 # prints "$2$1$3$1...$N"
 join() {
-	local sep="$1"; shift
-	local out; printf -v out "${sep//%/%%}%s" "$@"
-	echo "${out#$sep}"
+    local sep="$1"; shift
+    local out; printf -v out "${sep//%/%%}%s" "$@"
+    echo "${out#$sep}"
 }
 
 for version in "${versions[@]}"; do
 
-	for v in \
+    for v in \
         buster/{jdk-15,jdk-11,jdk-8} \
         windowsservercore-{1809,ltsc2016}/{jdk-15,jdk-11,jdk-8} \
     ; do
@@ -82,25 +82,25 @@ for version in "${versions[@]}"; do
         javaType="${javaVariant%-*}"
         dir="$version/$v"
 
-		[ -f "$dir/Dockerfile" ] || continue
+        [ -f "$dir/Dockerfile" ] || continue
 
-		commit="$(dirCommit "$dir")"
+        commit="$(dirCommit "$dir")"
 
-		versionAliases=(
-			$version
-			${aliases[$version]:-}
-		)
+        versionAliases=(
+            $version
+            ${aliases[$version]:-}
+        )
 
-		variantAliases=( "${versionAliases[@]/%/-$javaType$javaVersion-$os}" )
+        variantAliases=( "${versionAliases[@]/%/-$javaType$javaVersion-$os}" )
         variantAliases=( "${variantAliases[@]//latest-/}" )
 
-		case "$os" in
-			windows*) variantArches='windows-amd64' ;;
-			*)
-				variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
-				variantArches="${parentRepoToArches[$variantParent]}"
-				;;
-		esac
+        case "$os" in
+            windows*) variantArches='windows-amd64' ;;
+            *)
+                variantParent="$(awk 'toupper($1) == "FROM" { print $2 }' "$dir/Dockerfile")"
+                variantArches="${parentRepoToArches[$variantParent]}"
+                ;;
+        esac
 
         if [ "$javaVersion" = "$defaultJavaVersion" ]; then
             variantAliases+=( "${versionAliases[@]/%/-$javaType-$os}" )
@@ -108,10 +108,10 @@ for version in "${versions[@]}"; do
             variantAliases=( "${variantAliases[@]//latest-/}" )
         fi
 
-		sharedTags=()
+        sharedTags=()
 
-		if [ "$os" = "$defaultDebianSuite" ] || [[ "$os" == 'windowsservercore'* ]]; then
-			sharedTags+=( "${versionAliases[@]/%/-$javaType$javaVersion}" )
+        if [ "$os" = "$defaultDebianSuite" ] || [[ "$os" == 'windowsservercore'* ]]; then
+            sharedTags+=( "${versionAliases[@]/%/-$javaType$javaVersion}" )
             if [ "$javaVersion" = "$defaultJavaVersion" ]; then
                 sharedTags+=( "${versionAliases[@]/%/-$javaType}" )
                 sharedTags+=( "${versionAliases[@]}" )
@@ -120,20 +120,20 @@ for version in "${versions[@]}"; do
             if [[ "$os" == "windowsservercore"* ]]; then
                 sharedTags+=( "${sharedTags[@]/%/-windowsservercore}")
             fi
-		fi
+        fi
 
         sharedTags=( "${sharedTags[@]//latest-/}" )
 
-		echo
-		echo "Tags: $(join ', ' "${variantAliases[@]}")"
-		if [ "${#sharedTags[@]}" -gt 0 ]; then
-			echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
-		fi
-		cat <<-EOE
+        echo
+        echo "Tags: $(join ', ' "${variantAliases[@]}")"
+        if [ "${#sharedTags[@]}" -gt 0 ]; then
+            echo "SharedTags: $(join ', ' "${sharedTags[@]}")"
+        fi
+        cat <<-EOE
 			Architectures: $(join ', ' $variantArches)
 			GitCommit: $commit
 			Directory: $dir
 		EOE
-		[[ "$os" == "windows"* ]] && echo "Constraints: $os"
-	done
+        [[ "$os" == "windows"* ]] && echo "Constraints: $os"
+    done
 done
--- update.sh
+++ update.sh
@@ -6,13 +6,13 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
 versions=( "$@" )
 
 generated_warning() {
-	cat <<-EOH
-		#
-		# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
-		#
-		# PLEASE DO NOT EDIT IT DIRECTLY.
-		#
-	EOH
+    cat <<EOH
+#
+# NOTE: THIS DOCKERFILE IS GENERATED VIA "update.sh"
+#
+# PLEASE DO NOT EDIT IT DIRECTLY.
+#
+EOH
 }
 
 for version in "${versions[@]}"; do
@@ -20,10 +20,10 @@ for version in "${versions[@]}"; do
     abclBinZipUrl="https://abcl.org/releases/$version/abcl-bin-$version.zip"
     abclBinZipSha="$(curl -fsSL "$abclBinZipUrl" | sha256sum | cut -d' ' -f1)"
 
-	for v in \
+    for v in \
         buster/{jdk-15,jdk-11,jdk-8} \
         windowsservercore-{1809,ltsc2016}/{jdk-15,jdk-11,jdk-8} \
-	; do
+    ; do
         os="${v%%/*}"
         javaVariant="$(basename "$v")"
         javaVersion="${javaVariant#jdk-}"
@@ -32,27 +32,27 @@ for version in "${versions[@]}"; do
 
         mkdir -p "$dir"
 
-		case "$os" in
-			buster)
+        case "$os" in
+            buster)
                 template="apt"
                 cp abcl-wrapper "$dir/abcl-wrapper"
                 cp docker-entrypoint.sh "$dir/docker-entrypoint.sh"
                 ;;
-			windowsservercore-*)
+            windowsservercore-*)
                 template='windowsservercore'
                 cp abcl-wrapper.bat "$dir/abcl-wrapper.bat"
                 ;;
-		esac
+        esac
 
         tag="$javaVersion-$javaType-$os"
-		template="Dockerfile-${template}.template"
+        template="Dockerfile-${template}.template"
 
-		{ generated_warning; cat "$template"; } > "$dir/Dockerfile"
+        { generated_warning; cat "$template"; } > "$dir/Dockerfile"
 
-		sed -ri \
-			-e 's/^(ENV ABCL_VERSION) .*/\1 '"$version"'/' \
+        sed -ri \
+            -e 's/^(ENV ABCL_VERSION) .*/\1 '"$version"'/' \
             -e 's/^(ENV ABCL_SHA256) .*/\1 '"$abclBinZipSha"'/' \
-			-e 's/^(FROM) .*/\1 '"openjdk:$tag"'/' \
-			"$dir/Dockerfile"
-	done
+            -e 's/^(FROM) .*/\1 '"openjdk:$tag"'/' \
+            "$dir/Dockerfile"
+    done
 done
